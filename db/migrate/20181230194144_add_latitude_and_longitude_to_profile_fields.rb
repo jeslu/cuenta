36a50bace5ee12802c736dde7a48294fe4059119
guadalupe <mariaguadalupe.lopezlpz@gmail.com>
2019-05-29 23:25:53 -0500

    cambio de version de bulma

--- db/migrate/20181230194144_add_latitude_and_longitude_to_profile_fields.rb
+++
@@ -1,6 +0,0 @@
-class AddLatitudeAndLongitudeToProfileFields < ActiveRecord::Migration[5.1]
-  def change
-    add_column :profile_fields, :latitude, :float
-    add_column :profile_fields, :longitude, :float
-  end
-end
